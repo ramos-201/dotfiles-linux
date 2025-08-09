a14e759972fb611613c526d70b83c1ea93669c67
Ramos <andfera201@gmail.com>
2025-08-09 10:47:28 -0500

    snap isntall apps

---
+++ snap-install-app.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# ------------------------------------------------------------
+# Snap application installation script
+# Installs selected applications using Snap
+# ------------------------------------------------------------
+
+echo "Installing applications via Snap..."
+
+# ---------------------------
+# Install Obsidian
+# ---------------------------
+sudo snap install obsidian --classic
+
+echo "Snap applications installed successfully!"
