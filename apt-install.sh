69b8d1d33620e12ac3f5e97f3750c6913f09ddd8
Ramos <andfera201@gmail.com>
2025-08-11 13:08:57 -0500

    Add Zsh and oh-my-zsh

--- apt-install.sh
+++ apt-install.sh
@@ -19,14 +19,14 @@ sudo apt install gnome-shell-extension-manager
 
 # ---------------------------
 # Install Rclone
-# Command-line tool to manage and sync files across cloud storage services.
+# Command-line tool to manage and sync files across cloud storage services
 #
 # Open Rclone configuration:
 #   rclone config
 #
 # Configure Google Drive mount:
-#   - Open the Rclone configuration tool.
-#   - Follow the authentication instructions.
+#   - Open the Rclone configuration tool
+#   - Follow the authentication instructions
 #   - Create a mount point:
 #       mkdir ~/GoogleDrive
 #
@@ -39,7 +39,7 @@ sudo apt install rclone
 # ---------------------------
 # Install Tmux
 # Tmux is a terminal multiplexer: it enables a number of terminals
-# to be created, accessed, and controlled from a single screen.
+# to be created, accessed, and controlled from a single screen
 # Official website: github.com/tmux/tmux
 #
 # Open ui:
@@ -47,12 +47,15 @@ sudo apt install rclone
 # ---------------------------
 sudo apt install tmux
 
+# ---------------------------
 # Copy Tmux settings
 cp resources/tmux.conf ~/.tmux.conf
+# ---------------------------
 
 # ---------------------------
 # Install Alacritty
-#  Alacritty is a modern terminal emulator that comes with sensible defaults, but allows for extensive configuration
+# Alacritty is a modern terminal emulator that comes with sensible defaults,
+# but allows for extensive configuration
 # Official website: https://github.com/alacritty/alacritty/blob/master/INSTALL.md
 #
 # Open ui:
@@ -60,6 +63,24 @@ cp resources/tmux.conf ~/.tmux.conf
 # ---------------------------
 sudo apt install alacritty
 
+# ---------------------------
 # Copy Alacritty settings
+# ---------------------------
 mkdir -p ~/.config/alacritty
 cp /resources/alacritty.toml ~/.config/alacritty/alacritty.toml
+
+# ---------------------------
+# Install Zsh
+# Zsh is an advanced shell designed for interactive use,
+# with powerful features for customization and productivity
+# Official website: https://www.zsh.org/
+#
+# To start Zsh manually after installation:
+#   zsh
+# ---------------------------
+sudo apt install zsh
+
+# ---------------------------
+# Set Zsh as the default shell
+# ---------------------------
+chsh -s $(which zsh)
---
+++ install-oh-my-zsh.sh
@@ -0,0 +1,15 @@
+# ---------------------------
+# Install Oh-my-zsh
+# Oh My Zsh is an open source, community-driven framework
+# for managing your zsh configuration
+# Official website: https://github.com/ohmyzsh/ohmyzsh
+# ---------------------------
+# Install CURL
+# Command-line tool for transferring data
+sudo apt install curl
+# ---------------------------
+# Install GIT
+# Version control system
+sudo apt-get install git
+# ---------------------------
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
