0364959fd33bec7d370bef25b9c0b11a3f021742
ramos201 <andfera201@gmail.com>
2025-08-11 22:14:42 -0500

    Add Vim and aliases

---
+++ resources/aliases.zsh
@@ -0,0 +1,33 @@
+# -----------------------------------
+# General
+# -----------------------------------
+alias cls='clear'
+alias ll='ls -l'
+alias la='ls -la'
+alias .='cd'
+alias ..='cd ../'
+
+alias cdw='cd ~/Personal/Codes/WorkDev'
+alias cdp='cd ~/Personal/Codes/PersonalDev'
+
+# -----------------------------------
+# Git
+# -----------------------------------
+alias g='git'
+alias gs='git status -sb'
+alias gsu='git status -u'
+alias ga='git add'
+alias gaa='git add .'
+alias gc='git commit -v'
+alias gcm='git commit -m'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gp='git push'
+alias gpl='git pull'
+alias gcl='git clone'
+alias gb='git branch'
+alias gbd='git branch -d'
+alias gsta='git stash'
+alias gstp='git stash pop'
+alias gr='git remote -v'
+alias gcfg='git config --global'
