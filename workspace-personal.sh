951d7e3f9104f7771c7452796c0f3331122b9878
Ramos <andfera201@gmail.com>
2025-08-11 21:14:44 -0500

    Add workspace-personal.ah

---
+++ workspace-personal.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ---------------------------
+# Workspace Personal
+# Creates personal directories
+# ---------------------------
+
+# ---------------------------
+# Base personal folder
+# ---------------------------
+mkdir ~/Personal
+
+# ---------------------------
+# Folder for application developments
+# ---------------------------
+mkdir ~/Personal/Codes
+mkdir ~/Personal/Codes/PersonalDev
+mkdir ~/Personal/Codes/WorkDev
